4cc0edd7df9000b5c24f83f4349969a937181b37
Jack Keating <keatjm@gmail.com>
2016-02-12 01:04:23 -0600

    Pushing new changes

--- app/components/stream-search.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Component.extend({
-	actions: {
-		sendSearchAction(searchTerms) {
-			this.sendAction('search', searchTerms);
-		}
-	}
-});
--- app/helpers/link-to-profile.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-export function linkToProfile(params) {
-	let streamURL = params[0];
-	return (streamURL + "/profile");
-}
-
-export default Ember.Helper.helper(linkToProfile);
